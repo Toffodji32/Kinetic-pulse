3008282fda2002b2ae521845bbb36161eb57d23f
Toffodji32 <cedricatchade64@gmail.com>
2026-06-13 17:11:51 +0100

    Mise à jour de l'URL de base de l'API pour utiliser HTTPS

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -3,7 +3,7 @@ import { useAuthStore } from '../stores/auth';
 import router from '../router';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://127.0.0.1:8000/api',
+  baseURL: import.meta.env.VITE_API_URL || 'https://kenetic-pulse-api-production.up.railway.app',
 
 });
 console.log('API URL:', import.meta.env.VITE_API_URL)
