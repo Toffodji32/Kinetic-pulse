9ad506bc746b452152487706158f121067909139
Toffodji32 <cedricatchade64@gmail.com>
2026-06-16 18:28:52 +0100

    fix: switch EmailJS service from Gmail to Outlook

--- src/services/emailjs.js
+++ src/services/emailjs.js
@@ -1,7 +1,7 @@
 import emailjs from '@emailjs/browser'
 
 const PUBLIC_KEY = 'zGwfaP6kOxnr932Nj'
-const SERVICE_ID = 'service_fuyfg9i'
+const SERVICE_ID = 'service_8dm8nqh'
 const TEMPLATE_ID = 'template_i5bz0rl'
 
 emailjs.init(PUBLIC_KEY)
